d1d37f03eb35425989cb6f75becbe27f61300d26
Ashehal Sadik <ashehalomarsadik@gmail.com>
2026-02-28 13:26:00 +0500

    incorporate Spring Security and capture user login in to query database

--- src/main/java/pk/edu/nu/isb/bms/controllers/ContentController.java
+++ src/main/java/pk/edu/nu/isb/bms/controllers/ContentController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class contentController {
+public class ContentController {
 
     @GetMapping("/login")
     public String login() {
---
+++ src/main/java/pk/edu/nu/isb/bms/models/MyUserService.java
@@ -0,0 +1,40 @@
+package pk.edu.nu.isb.bms.models;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class MyUserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public MyUser saveUser(MyUser user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<MyUser> user = userRepository.findByUsername(username);
+        if (user.isPresent()) {
+            var userObj = user.get();
+            return User.builder()
+                    .username(userObj.getUsername())
+                    .password(userObj.getPassword())
+                    .build();
+        }
+        else {
+            throw new UsernameNotFoundException(username);
+        }
+
+    }
+}
